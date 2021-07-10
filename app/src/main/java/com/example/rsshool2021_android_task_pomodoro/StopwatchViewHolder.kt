eac24b91dfa0669c92639b231392891db6aeae2d
mixei4ik <mixei4ik87@gmail.com>
2021-07-10 14:03:28 +0300

    add: 03. Stopwatch

--- app/src/main/java/com/example/rsshool2021_android_task_pomodoro/StopwatchViewHolder.kt
+++ app/src/main/java/com/example/rsshool2021_android_task_pomodoro/StopwatchViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.rsshool2021_android_task_pomodoro
 
+import android.os.CountDownTimer
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rsshool2021_android_task_pomodoro.databinding.StopwatchItemBinding
 
@@ -7,8 +8,35 @@ class StopwatchViewHolder (
     private val binding: StopwatchItemBinding
     ): RecyclerView.ViewHolder(binding.root) {
 
+        private var timer: CountDownTimer? = null
+
         fun bind(stopwatch: Stopwatch) {
             binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+
+            if (stopwatch.isStarted) {
+                startTimer(stopwatch)
+            }
+        }
+
+        private fun startTimer(stopwatch: Stopwatch) {
+            timer?.cancel()
+            timer = getCountDownTimer(stopwatch)
+            timer?.start()
+        }
+
+        private fun getCountDownTimer(stopwatch: Stopwatch): CountDownTimer {
+            return object : CountDownTimer(PERIOD, UNIT_TEN_MS) {
+                val interval = UNIT_TEN_MS
+
+                override fun onTick(millisUntilFinished: Long) {
+                    stopwatch.currentMs +=interval
+                    binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+                }
+
+                override fun onFinish() {
+                    binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+                }
+            }
         }
 
         private fun Long.displayTime(): String {
@@ -34,5 +62,7 @@ class StopwatchViewHolder (
         private companion object {
 
             private const val START_TIME = "00:00:00:00"
+            private const val UNIT_TEN_MS = 10L
+            private const val PERIOD = 1000L * 60L * 60L * 24L
         }
 }
